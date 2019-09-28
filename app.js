8134a5f2ae73eac2f7609e142564ce61df3e55cf
GilbertRoss <bhuiyannazrul74@gmail.com>
2019-09-28 10:07:36 +0200

    upload project

---
+++ app.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', () => {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            console.log(lat);
+            console.log(long);
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = proxy + "https://api.darksky.net/forecast/37c53c277cfd91db0496877661bf2e87/" + lat + "," + long + "?units=auto&lang=it";
+            console.log(api);
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    const { temperature, summary, icon } = data.currently;
+                    //Set DOM Elements from the API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                    //set Icons
+                    setIcons(icon, document.querySelector('.icon'));
+
+
+                });
+        });
+
+
+    }
+
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({ color: "white" });
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+});
